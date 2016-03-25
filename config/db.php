517046fede34dfe6111d9add9797bf9b00e3afb4
Kravtsov <mashimp@mail.ru>
2016-03-25 15:56:44 +0300

    Create table table_user and create model User

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=lesson2',
+    'dsn' => 'mysql:host=localhost;dbname=yii_lessons_data',
     'username' => 'root',
     'password' => 'gaantanta',
     'charset' => 'utf8',
