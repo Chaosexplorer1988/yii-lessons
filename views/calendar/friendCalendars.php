7b95572e8d167cb4c0a80856d4928582a78ae6ad
Kravtsov <mashimp@mail.ru>
2016-04-07 13:34:43 +0300

    Add friends access

---
+++ views/calendar/friendCalendars.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ritor
+ * Date: 06.04.16
+ * Time: 11:28
+ */
---
+++ views/calendar/viewGuest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ritor
+ * Date: 05.04.16
+ * Time: 20:38
+ */
